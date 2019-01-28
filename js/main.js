01a8f762a1ebae4f9c28414942234e54751b06b4
Jhurgen Maraza <jhurgenmrz99@gmail.com>
2019-01-28 11:11:20 -0300

    Obtenengo el valor de los inputs por Jquery

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+let description = $('#description')
+let carbs = $('#carbs')
+let calories = $('#calories')
+let protein = $('#protein')
+
+// Object.assign({},"Objeto_a_copiar")  
+// Esto es para copiar un objeto, 
+// no la referencia sino el valor del objeto aunq solo sirve para un objeto de primer nivel
+ 
+//JSON.parse() y JSON.stringify()  
+// Esto funciona de la siguiente forma
+//JSON.parse(JSON.stringify("OBJETO_A_COPIAR"))
+//Ej. let new Car = JSON.parse(JSON.stringify(car))
